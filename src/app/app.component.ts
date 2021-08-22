5ee3b04151f8a1b8144b847b3f770d9d9840ea08
Yonatan Leonel Baeza Zepeda <a20170232@utem.edu.mx>
2021-08-21 19:13:41 -0500

    Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetdataService } from './getdata.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular';
+
+  tittle = 'Obtener datos JSON';
+  public listOfData:any=[]
+
+  constructor(private GetdataService:GetdataService){
+
+  }
+
+  ngOnInit():void {
+    this.loadData();
+  }
+
+  public loadData(){
+    this.GetdataService.get('https://jsonplaceholder.typicode.com/posts').subscribe(data=>{
+      console.log(data);
+      this.listOfData = data;
+    })
+  }
+
 }
